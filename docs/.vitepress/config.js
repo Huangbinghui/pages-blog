e63c6a0bb9403188a84196fe87a674cef0d28fee
huangbh <herberth.huang@icloud.com>
2024-03-31 22:22:29 +0800

    init

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,99 +1,47 @@
 export default {
-  title: '辉辉', // 博客的标题
-  description: '辉辉 的个人博客', // 博客的介绍
-  base: '/blog/', // 如果想用 https://mlyz.wdy.github.io/blog/ 访问，那么这句话必填
+  title: "辉辉", // 博客的标题
+  description: "辉辉 的个人博客", // 博客的介绍
+  base: "/blog/", // 如果想用 https://mlyz.wdy.github.io/blog/ 访问，那么这句话必填
   themeConfig: {
-      logo: "/images/logo.png", // 页面上显示的logo
-      nav: [ // 页面右上角的导航
-          { text: "vue", link: "/articles/vue/上传素材到COS" },
-          { text: "uniapp", link: "/articles/uniapp/一键登录" },
-          {
-              text: '博客文档',
-              items: [ // 可以配置成下拉
-                  { text: 'JavaScript 核心系列', link: '/articles/javaScript-core/构造函数、原型、原型链' },
-                  { text: 'Vue 三方组件库', link: '/articles/libs/VForm3低代码初体验' },
-                  { text: '其他', link: '/articles/other/nvm管理node' },
-              ]
-          }
-      ],
-      sidebar: { // 侧边栏，可以分组
-          "/articles/vue/": [
-              {
-                  text: "基础",
-                  items: [
-                  ],
-              },
-              {
-                  text: "代码段",
-                  items: [
-                      {
-                          text: "上传素材到COS",
-                          link: "/articles/vue/上传素材到COS",
-                      },
-                      {
-                          text: "文件下载",
-                          link: "/articles/vue/文件下载",
-                      },
-                  ],
-              },
-          ],
-          "/articles/uniapp/": [
-              {
-                  text: "基础",
-                  items: [
-                  ],
-              },
-              {
-                  text: "代码段",
-                  items: [
-                      {
-                          text: "一键登录",
-                          link: "/articles/uniapp/一键登录",
-                      }
-                  ],
-              },
-          ],
-          "/articles/javaScript-core/": [
-              {
-                  text: "基础",
-                  items: [
-                  {
-                      text: "1. 构造函数、原型、原型链",
-                      link: "/articles/javaScript-core/构造函数、原型、原型链",
-                  },
-                  {
-                      text: "2. 执行上下文和执行上下文栈",
-                      link: "/articles/javaScript-core/执行上下文和执行上下文栈",
-                  },
-                  {
-                      text: "3. this的指向",
-                      link: "/articles/javaScript-core/this的指向",
-                  },
-                  ],
-              },
-              {
-                  text: "进阶",
-                  items: [
-                  {
-                      text: "xx",
-                      link: "/xx",
-                  },
-                  ],
-              },
-          ],
-          "/articles/libs/": [
-              {
-                  items: [
-                  {
-                      text: "1. VForm3低代码初体验",
-                      link: "/articles/libs/VForm3低代码初体验",
-                  },
-                  ],
-              }
-          ],
+    logo: "/images/logo.png", // 页面上显示的logo
+    nav: [
+      // 页面右上角的导航
+      { text: "计算机基础", link: "/articles/CS/index" },
+      { text: "前端", link: "/articles/frontend/index" },
+      {
+        text: "JAVA",
+        items: [
+          // 可以配置成下拉
+          { text: "泛型", link: "/articles/Java/泛型/类型擦除" },
+          { text: "算法", link: "/articles/Java/算法/查找" },
+          { text: "中间件", link: "/articles/Java/中间件/redis/HighAvailable" },
+        ],
       },
-      socialLinks: [{ icon: "github", link: "https://github.com/Huangbinghui" }], // 可以连接到 github
+    ],
+    sidebar: {
+      "/articles/CS/": [
+        {
+          text: "计算机基础",
+          collapsed: true,
+          items: [
+            { text: "pg", link: "/articles/CS/pg" },
+            { text: "vim", link: "/articles/CS/vim/vim使用技巧" },
+          ],
+        },
+      ],
+      "/articles/Java/": [
+        {
+          text: "Java",
+          collapsed: true,
+          items: [
+            { text: "泛型", link: "/articles/Java/泛型/类型擦除" },
+            { text: "算法", link: "/articles/Java/算法/查找" },
+            { text: "中间件", link: "/articles/Java/中间件/redis/HighAvailable" },
+          ],
+        },
+      ],
+    },
+    socialLinks: [{ icon: "github", link: "https://github.com/Huangbinghui" }], // 可以连接到 github
   },
-  ignoreDeadLinks:true,
-}
-
+  ignoreDeadLinks: true,
+};
